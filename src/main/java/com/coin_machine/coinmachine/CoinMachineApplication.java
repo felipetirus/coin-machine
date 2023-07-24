c0e422f842bc9bc483300814d8a7b6a249ae63fe
Felipe Andrighetti <felipetirus@gmail.com>
2023-07-24 11:34:17 -0400

    Spring boot project

---
+++ src/main/java/com/coin_machine/coinmachine/CoinMachineApplication.java
@@ -0,0 +1,13 @@
+package com.coin_machine.coinmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coin_machine/coinmachine/CoinMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coin_machine.coinmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
